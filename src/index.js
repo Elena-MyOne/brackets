5b39424cd15531ee1fcf7564635278fae488b89e
Elena-MyOne <yakovenko.ea72@gmail.com>
2023-08-08 19:07:57 -0400

    add solution for brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length % 2 !== 0) return false;
+  if (bracketsConfig[0][0] !== str[0] && bracketsConfig[1][0] !== str[0]) return false;
+
+  const result = [];
+
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (bracketsConfig[j][0] === str[i]) {
+        result.push(str[i]);
+      }
+
+      if (bracketsConfig[j][1] === str[i] && bracketsConfig[j][0] === result[result.length - 1]) {
+        if (str[0] === '|' && str[str.length - 1] !== '|') {
+          continue;
+        } else {
+          result.pop();
+        }
+      }
+    }
+  }
+
+  return result.length === 0;
+};
